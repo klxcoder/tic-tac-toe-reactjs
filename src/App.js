c0eda6019e1ad0177cdd2f706c606cee21e6cc08
klxcoder <klxcoder@gmail.com>
2025-03-15 10:33:39 +0700

    add states to the component Game

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function Square({ value, onSquareClick }) {
   >{value}</button>;
 }
 
-function Board() {
-
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
@@ -17,11 +15,8 @@ function Board() {
     }
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O"
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
   const winner = calculateWinner(squares);
   const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? "X" : "O"}`;
@@ -49,10 +44,23 @@ function Board() {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+        />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
